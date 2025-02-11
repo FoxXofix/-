030ecfa2b1f5264c87d26bcf81fef82fe423eae5
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-11 16:53:33 +0300

    Добавьте файлы проекта.

---
+++ сравнение периметра и площади/сравнение периметра и площади.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+void fun(float x, float& p, float& s)
+{
+    p = x * 4;
+    s = x * x;
+}
+
+int main()
+{
+    float a, p, s;
+    cout << "a = ? \n";
+    cin >> a;
+    fun(a, p, s);
+    cout << (p >= s ? "p > s" : "s < p") << endl;
+    return 0;
+}
